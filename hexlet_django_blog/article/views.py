0e3d7b972280eb4ceaf7592efb57fddbe32437d2
Andrey Solovyov <solovyov.net@gmail.com>
2025-10-15 23:22:14 +0300

    add views

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 
 # def index(request):
 #     return HttpResponse("article")
 # Create your views here.
 
-def index(request):
-    app_name = 'article'
-    return render(
-        request,
-        "articles/index.html",
-        context={
-            "app_name": app_name,
-        },
-    )
+# def index(request):
+#     app_name = 'article'
+#     return render(
+#         request,
+#         "articles/index.html",
+#         context={
+#             "app_name": app_name,
+#         },
+#     )
+
+class index(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["app_name"] = 'article'
+        return context
