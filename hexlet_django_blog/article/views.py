49b5240362693c5a64b31839ada89424028e6c0f
Andrey Solovyov <solovyov.net@gmail.com>
2025-10-15 19:36:35 +0300

    add new template

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("article")
+# def index(request):
+#     return HttpResponse("article")
 # Create your views here.
+
+def index(request):
+    app_name = 'article'
+    return render(
+        request,
+        "articles/index.html",
+        context={
+            "app_name": app_name,
+        },
+    )
